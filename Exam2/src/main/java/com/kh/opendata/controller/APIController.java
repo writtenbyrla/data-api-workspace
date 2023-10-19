d04c75f5479d837b7fc5cbaa1c86465619615fb6
writtenbyrla <gnnvistavie@gmail.com>
2023-10-19 17:50:08 +0900

    공공데이터 api 활용

---
+++ Exam2/src/main/java/com/kh/opendata/controller/APIController.java
@@ -0,0 +1,54 @@
+package com.kh.opendata.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class APIController {
+	public static final String serviceKey = "B89IuQPz4Ros4mmF86h9rCpFFjNL%2Bmi%2FnnjSEhyRRqVRyBqoP3ePHSrjoAwS0sGIhvPVTmbS8q0q8EtQV%2FQqxg%3D%3D";
+	
+	@ResponseBody
+	@RequestMapping(value="api.do", produces="application/json; charset=utf-8")
+	public String apiInfo(String stnId)  throws IOException{
+		// URL (key값 제외한 주소)
+		String url = "https://apis.data.go.kr/1360000/MidFcstInfoService/getMidFcst";
+		
+		url += "?serviceKey=" + serviceKey; 
+		url += "&tmFc=202310190600";
+		url += "&dataType=JSON"; // 기본값 xml, json으로 지정 가능
+		url += "&stnId=" + stnId; // 지점번호(front에서 파라미터값으로 넘겨받아서 호출)
+		
+		// 파라미터 값이 한글이면 utf-8로 인코딩 해야함
+		//String stnName = "서울";
+		//url += "stnName=" + URLEncoder.encode(stnName, "UTF-8");
+		
+		
+		URL requestUrl = new URL(url);
+		HttpURLConnection urlConnection = (HttpURLConnection) requestUrl.openConnection();
+		urlConnection.setRequestMethod("GET"); // 요청 방식
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String line = null;
+		
+		String responseText = "";
+		
+		while((line = br.readLine())!=null) {
+			responseText += line; // responseText에 한줄씩 담아서 리턴
+		} 
+		
+		br.close();
+		urlConnection.disconnect();
+		
+		System.out.println(responseText);
+		
+		return responseText;
+	}
+}
---
+++ Exam2/src/main/webapp/index.jsp
@@ -0,0 +1,73 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<h1>기상청 예보</h1>
+	
+	<%-- 만약에 option에 value를 지정하지 않으면 태그내 정보가 value! --%>
+	지역:
+	<select id="stnId">
+		<option value="108">전국</option>
+		<option value="109">서울, 인천, 경기도</option>
+		<option value="156">광주, 전라남도</option>		
+		<option value="184">제주도</option>
+	</select>
+	<button id="btn">해당 지역 예보 정보</button>
+	
+	<br><br>
+	
+	<table border="1">
+		<thead>
+			<tr>
+				<th>예보 정보</th>
+			</tr>
+		</thead>
+		<tbody>
+			
+		</tbody>
+	</table>
+	
+	<script>
+		$(function(){
+			$('#btn').click(function() {
+				$.ajax({
+					url: "api.do",
+					data: {stnId: $("#stnId").val()},
+					success: function(data){
+						console.log(data);
+						console.log(data.response.body.items.item[0].wfSv); // 여러 데이터인 경우에는 for문 돌려야 함
+						let value = "";
+						
+						const result = data.response.body.items.item;
+						//console.log("result : " + result);
+						
+						// 데이터 쪼개기 위한 구분자
+						const dataList = result[0].wfSv.split("○ ");
+						//console.log("List : " + dataList);
+						
+						for(let index in dataList){
+							console.log(dataList[index]);
+						
+							if(dataList[index] !== ""){
+								value += "<tr>"
+									+ "<td>" + dataList[index] + "</td>"
+									+ "</tr>";
+							}
+					}
+
+					
+					$("table tbody").html(value);
+					}
+				})
+			});
+		});
+	</script>
+</body>
+</html>
